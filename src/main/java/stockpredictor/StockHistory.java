a2330e4419b248d0b8b68757c4a0dac1b42909db
Jordan Lavigueure <lav.jordan@live.com>
2023-09-04 22:44:26 -0700

    added ability to export list of certain data (highs, lows, volumes, etc.)

--- src/main/java/stockpredictor/StockHistory.java
+++ src/main/java/stockpredictor/StockHistory.java
@@ -16,6 +16,54 @@ public class StockHistory {
 		return history;
 	}
 	
+	public List<Double> getOpens(){
+		List<Double> out = new ArrayList<>();
+		for(StockDay day : history) {
+			out.add(day.getOpen());
+		}
+		return out;
+	}
+	
+	public List<Double> getHighs(){
+		List<Double> out = new ArrayList<>();
+		for(StockDay day : history) {
+			out.add(day.getHigh());
+		}
+		return out;
+	}
+	
+	public List<Double> getLows(){
+		List<Double> out = new ArrayList<>();
+		for(StockDay day : history) {
+			out.add(day.getLow());
+		}
+		return out;
+	}
+	
+	public List<Double> getClosings(){
+		List<Double> out = new ArrayList<>();
+		for(StockDay day : history) {
+			out.add(day.getClose());
+		}
+		return out;
+	}
+	
+	public List<Double> getAdjClosings(){
+		List<Double> out = new ArrayList<>();
+		for(StockDay day : history) {
+			out.add(day.getAdjClose());
+		}
+		return out;
+	}
+	
+	public List<Integer> getVolumes(){
+		List<Integer> out = new ArrayList<>();
+		for(StockDay day : history) {
+			out.add(day.getVolume());
+		}
+		return out;
+	}
+	
 	//--- Modifiers ---
 	public void setHistory(List<StockDay> history) {
 		this.history = history;
